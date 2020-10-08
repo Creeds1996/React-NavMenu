61d2dc0dfd36207f6bbfad3922b6fa152c868c6a
Nathan <Creeds123@hotmail.com>
2020-10-08 19:31:42 +1100

    Styling Change
    Changed the content from a h3 tag to a span and resized font.

--- MenuItem.js
+++ MenuItem.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
 const MenuItem = (props) => {
   return (
-    <NavLink to={props.Url || '/'} className={`MenuItem ${props.className}`}>
-      <h3>{props.Title}</h3>
+    <NavLink to={props.Url || "/"} className={`MenuItem ${props.className}`}>
+      <span className="MenuItem-Title">{props.Title}</span>
     </NavLink>
   );
 };
